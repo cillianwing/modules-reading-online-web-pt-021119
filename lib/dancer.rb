7d06ccaa0009ce2eb470a4cb968fdef4ad2ff692
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-27 00:43:06 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,16 +2,8 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer 
-  #include Dance #methods from Dance can be used as instance methods
-  
-  extend MetaDancing #methods from MetaDancing can be used as class methods
-  
-=begin
-  attr_accessor :name 
-  
-  def initialize(name)
-    @name = name 
-  end
-=end
+  extend
+  FancyDance::ClassMethods 
+  include 
   
 end
