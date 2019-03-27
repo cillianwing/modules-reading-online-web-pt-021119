aa44ba8308de1af8cbc4e85e8655004cfa57537c
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-27 00:39:15 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,7 +3,9 @@ require_relative './class_methods_module.rb'
 
 class Kid 
   #include Dance #methods from Dance can be used as instance methods
+  
   extend MetaDancing #methods from MetaDancing can be used as class methods
+  
 =begin
   attr_accessor :name 
   
