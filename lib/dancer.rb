6cab7ef0461f6ccbc8510421d20ce09f911ff89d
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-27 00:35:10 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer 
-  include Dance 
+  include Dance #methods from Dance can be used as instance methods
   
   attr_accessor :name 
   
