b29409bc283c0f24ace51f90c08e428cdceb54bb
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-27 00:35:24 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,7 +2,7 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid 
-  include Dance
+  include Dance #methods from Dance can be used as instance methods
   
   attr_accessor :name 
   
