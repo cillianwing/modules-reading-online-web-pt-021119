8e6e45b11cee7d686cc089b8c1d6ea21585ccee3
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-27 00:38:34 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,8 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer 
-  include Dance #methods from Dance can be used as instance methods
+  #include Dance #methods from Dance can be used as instance methods
+  extend MetaDancing #methods from MetaDancing can be used as class methods
   
   attr_accessor :name 
   
