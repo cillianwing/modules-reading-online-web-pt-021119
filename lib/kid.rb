edf6d3bb3123b994e0271616302f6b033757f60e
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-03-27 00:39:05 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -4,11 +4,11 @@ require_relative './class_methods_module.rb'
 class Kid 
   #include Dance #methods from Dance can be used as instance methods
   extend MetaDancing #methods from MetaDancing can be used as class methods
-  
+=begin
   attr_accessor :name 
   
   def initialize(name)
     @name = name 
   end
-
+=end
 end
